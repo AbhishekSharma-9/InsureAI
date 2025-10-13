2eb0be52eabf731166f7723a6b1d85b4ce6b4cfd
Abhishek Sharma <165680680+AbhishekSharma-9@users.noreply.github.com>
2025-10-14 01:36:11 +0530

    Create IfElse.java

---
+++ JavaExcercises/Conditional_Programs/IfElse.java
@@ -0,0 +1,13 @@
+package Infosys_SpringBoard.JavaExcercises.Conditional_Programs;
+
+public class IfElse {
+    public static void main(String[] args) {
+        int number = 13;
+
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
+        }
+    }
+}

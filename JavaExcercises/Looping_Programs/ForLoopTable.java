e3dec3c7dceaa64ae267b426cca10a56aa2620f9
Abhishek Sharma <165680680+AbhishekSharma-9@users.noreply.github.com>
2025-10-14 01:41:15 +0530

    Create ForLoopTable.java

---
+++ JavaExcercises/Looping_Programs/ForLoopTable.java
@@ -0,0 +1,11 @@
+package Infosys_SpringBoard.JavaExcercises.Looping_Programs;
+
+public class ForLoopTable {
+    public static void main(String[] args) {
+        int number = 5;
+        System.out.println("Multiplication Table for " + number);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+    }
+}

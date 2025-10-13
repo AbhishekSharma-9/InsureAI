11b999a9fe1e79ed1257688657c7eb97715ae9f5
Abhishek Sharma <165680680+AbhishekSharma-9@users.noreply.github.com>
2025-10-14 01:46:41 +0530

    Create PrintArray.java

---
+++ JavaExcercises/Array_Programs/PrintArray.java
@@ -0,0 +1,12 @@
+package Infosys_SpringBoard.JavaExcercises.Array_Programs;
+
+public class PrintArray {
+    public static void main(String[] args) {
+        String[] fruits = {"Apple", "Banana", "Cherry", "Date"};
+
+        System.out.println("The fruits in the array are:");
+        for (String fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+}

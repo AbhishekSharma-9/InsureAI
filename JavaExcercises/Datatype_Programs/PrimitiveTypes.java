99df94328814a0a40a97a036dd883a4473d0bd53
Abhishek Sharma <165680680+AbhishekSharma-9@users.noreply.github.com>
2025-10-14 01:19:50 +0530

    Create PrimitiveTypes.java

---
+++ JavaExcercises/Datatype_Programs/PrimitiveTypes.java
@@ -0,0 +1,25 @@
+package Infosys_SpringBoard.JavaExcercises.Datatype_Programs;
+
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        byte Byte = 100;
+        short Short = 30000;
+        int Int = 100000;
+        long Long = 9000000000L;
+
+        float Float = 3.14f;
+        double Double = 99.999;
+
+        char Char = 'Z';
+        boolean Boolean = true;
+
+        System.out.println("byte: " + Byte);
+        System.out.println("short: " + Short);
+        System.out.println("int: " + Int);
+        System.out.println("long: " + Long);
+        System.out.println("float: " + Float);
+        System.out.println("double: " + Double);
+        System.out.println("char: " + Char);
+        System.out.println("boolean: " + Boolean);
+    }
+}

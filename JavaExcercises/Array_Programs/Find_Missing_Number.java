9598bd7cefb12cc2fef2ccc34fb4d9921702503e
Abhishek Sharma <165680680+AbhishekSharma-9@users.noreply.github.com>
2025-10-15 12:36:50 +0530

    Create Find_Missing_Number.java

---
+++ JavaExcercises/Array_Programs/Find_Missing_Number.java
@@ -0,0 +1,37 @@
+package Striver.Array;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Find_Missing_Number {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 6, 7, 5, 7};
+        System.out.println(Arrays.toString(findMissingRepeatingNumbers(arr)));
+    }
+
+    public static int[] findMissingRepeatingNumbers(int[] nums) {
+        int j = 0;
+        while (j < nums.length) {
+            int correct = nums[j] - 1;
+            if (nums[j] != nums[correct]) {
+                int temp = nums[j];
+                nums[j] = nums[correct];
+                nums[correct] = temp;
+            } else {
+                j++;
+            }
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i]!=i+1){
+                list.add(nums[i]);
+                list.add(i+1);
+            }
+        }
+        int[] help = new int[list.size()];
+        for (int i = 0; i < help.length; i++) {
+            help[i] = list.get(i);
+        }
+        return help;
+    }
+}

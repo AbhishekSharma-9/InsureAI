a06f4a9eeabf264c4e9f6c40c5311cf23445fb8f
Abhishek Sharma <165680680+AbhishekSharma-9@users.noreply.github.com>
2025-10-24 17:28:03 +0530

    Add files via upload

---
+++ LoginScreen/src/App.jsx
@@ -0,0 +1,13 @@
+// File: src/App.jsx
+
+import React from 'react';
+import Login from './Login.jsx'; // Import the component we just made
+
+function App() {
+  // Return our Login component
+  return (
+    <Login />
+  );
+}
+
+export default App;
